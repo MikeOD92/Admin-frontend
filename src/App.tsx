3e1565ead5297865321b8689413bf3551b6658b1
Michael <michaelodell660@gmail.com>
2021-08-24 14:08:38 -0700

    finsihed edit roles component

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import RedirectToDashboard from './secure/RedirectToDashBoard';
 import { UserEdit } from './secure/users/UserEdit';
 import Roles from './secure/roles/Roles';
 import RoleCreate from './secure/roles/RoleCreate';
+import RoleEdit from './secure/roles/RoleEdit';
 
 function App() {
   return (
@@ -30,6 +31,7 @@ function App() {
               <Route path={'/users/:id/edit'} component={UserEdit} />
               <Route path={'/roles'} component={Roles} exact />
               <Route path={'/roles/create'} component={RoleCreate} />
+              <Route path={'/roles/:id/edit'} component={RoleEdit} />
 
 
             </BrowserRouter>
